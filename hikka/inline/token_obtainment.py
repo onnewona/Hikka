19f75f86b1f77b88fa11bb7c6b34b31f5a98a152
Onne Wona <104727779+onnewona@users.noreply.github.com>
2022-06-18 17:57:43 +0500

    Update token_obtainment.py

--- hikka/inline/token_obtainment.py
+++ hikka/inline/token_obtainment.py
@@ -48,14 +48,14 @@ class TokenObtainment(InlineUnit):
                 else:
                     # Generate and set random username for bot
                     uid = utils.rand(6)
-                    username = f"@hikka_{uid}_bot"
+                    username = f"@soso_o_bot"
             else:
                 # Generate and set random username for bot
                 uid = utils.rand(6)
-                username = f"@hikka_{uid}_bot"
+                username = f"@soso_o_bot"
 
             for msg in [
-                f"🌘 Hikka Userbot of {self._name}"[:64],
+                f"◍ soso ◍"[:64],
                 username,
                 "/setuserpic",
                 username,
@@ -149,7 +149,7 @@ class TokenObtainment(InlineUnit):
                         "hikka.inline",
                         "custom_bot",
                         False,
-                    ) and not re.search(r"@hikka_[0-9a-zA-Z]{6}_bot", button.text):
+                    ) and not re.search(r"@soso_o_bot", button.text):
                         continue
 
                     m = await conv.send_message(button.text)
@@ -192,7 +192,7 @@ class TokenObtainment(InlineUnit):
                     for msg in [
                         "/setinline",
                         button.text,
-                        "HikkaQuery",
+                        "sosooooooo",
                         "/setinlinefeedback",
                         button.text,
                         "Enabled",
