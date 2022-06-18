defe5f66bb14aecb17a1e847f8a7c72a91dde124
Onne Wona <104727779+onnewona@users.noreply.github.com>
2022-06-18 22:44:30 +0500

    Update query_gallery.py

--- hikka/inline/query_gallery.py
+++ hikka/inline/query_gallery.py
@@ -116,7 +116,7 @@ class QueryGallery(InlineUnit):
                     title=i["title"],
                     description=i["description"],
                     input_message_content=InputTextMessageContent(
-                        f"🌘 <b>Loading Hikka gallery...</b>\n<i>#id: {id_}</i>",
+                        f"<b>◽ soso gallery</b> - <code>#id: {id_}</code>",
                         "HTML",
                         disable_web_page_preview=True,
                     ),
